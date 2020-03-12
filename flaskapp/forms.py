ecbecb9b311ce395318a50f85d756ea393b88076
Vaibhav <vaibhavdaiict@gmail.com>
2020-03-12 12:32:28 +0530

    Now it can show question and update marks

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
+from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange,Optional
 
 
 class QuestionForm(FlaskForm):
@@ -10,4 +10,10 @@ class QuestionForm(FlaskForm):
                         validators=[DataRequired(), NumberRange(1, 100, "Not in a valid mark range")])
     difficulty = IntegerField('Difficulty',
                               validators=[DataRequired(), NumberRange(1, 100, "Not a valid difficulty")])
+    submit = SubmitField('submit')
+class updateForm(FlaskForm):
+    mark = IntegerField('Mark',
+                        validators=[NumberRange(1, 100, "Not in a valid mark range"),Optional()])
+    difficulty = IntegerField('Difficulty',
+                              validators=[NumberRange(1, 100, "Not a valid difficulty"),Optional()])
     submit = SubmitField('submit')
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect,request
 from flaskapp import app, db
-from flaskapp.forms import QuestionForm
+from flaskapp.forms import QuestionForm,updateForm
 from flaskapp.models import Question
 
 
@@ -21,3 +21,30 @@ def add_question():
         flash(f"New question added successfully!", "success")
         return redirect(url_for("add_question"))
     return render_template("question.html", form=form)
+
+@app.route("/show_questions") #List all the questions in database and when clicked on question it goes to update question
+def show_question():
+    q = db.session.query(Question).all() 
+    return render_template('list_question.html',questions = q) 
+
+@app.route("/update_question",methods=["GET","POST"]) 
+def update_question():
+    question_id = request.args.get('question_id')
+    que = db.session.query(Question).filter_by(id = question_id).first()
+    if que == None:
+        flash(f"Question Does not exist", "Failure")
+        return redirect(url_for("show_question"))
+    form = updateForm()
+    if form.validate_on_submit():        
+        if form.mark.data is not None:
+            que.mark = form.mark.data
+        if form.difficulty.data is not None:
+            que.difficulty = form.difficulty.data
+        db.session.commit()
+        flash(f"Update successfull!", "success")
+        return redirect(url_for("show_question"))
+    return render_template('updateMarkDifficulty.html',form = form,que = que)
+
+
+
+
