2bb9d0ac2421fc7db08bcdac9304796ed42549d0
Niraj Kamdar <nirajkamdar27@pm.me>
2020-03-15 11:52:56 +0530

    removed unnecessary import

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Length, NumberRange, Optional
+from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class QuestionForm(FlaskForm):
