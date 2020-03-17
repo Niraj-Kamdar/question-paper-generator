1e8eff14cf3f2aa39c40e00bdc96bcaa4ed3d0e2
Niraj Kamdar <nirajkamdar27@pm.me>
2020-03-17 15:58:06 +0530

    Update __init__.py and manage.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
