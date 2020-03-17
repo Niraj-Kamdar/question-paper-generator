58c8e528b27ad3ed267b6392b4ee8af3a29566a4
NIK-1999 <kambariyaniks9999@gmail.com>
2020-03-17 15:31:14 +0530

    Update index.js

--- flaskapp/static/js/index.js
+++ flaskapp/static/js/index.js
@@ -4,7 +4,7 @@ function scrollFunction() {
 	var color2 = document.getElementById("data_color2").innerHTML;
 	var header = document.getElementById("header");
 	var ofset = 50;
-	if ( window.pageYOffset > ofset || document.body.scrollTop > ofset || document.documentElement.scrollTop > ofset) {
+	if( window.pageYOffset > ofset || document.body.scrollTop > ofset || document.documentElement.scrollTop > ofset) {
 		header.classList.add("py-2");
 		header.classList.remove("py-4");
 		header.style.backgroundImage = "linear-gradient(to right, " +color2+ ", "+ color1 + ")";
@@ -15,32 +15,32 @@ function scrollFunction() {
 	}
 }
 window.onload=function(){
-    var loginbtn = document.getElementById("show_login");
-    loginbtn.addEventListener("click", () => {
-	    var x = document.getElementById('content');
-		if (x.className === 'hidden') {
-		    document.getElementById('bodycontent').style.marginLeft = "0%";
-		    document.getElementsByClassName('overlape')[0].style.marginLeft = "0%";
-		    document.getElementsByClassName('col2')[0].style.width = "85%";
-		    document.getElementById('footer').style.marginLeft = "0%";
-		    document.getElementById('footercontent').style.marginLeft = "0%";
-		    x.className = '';
-		    disp(x);
-		} else {
-		    x.className = 'hidden';
-		    setTimeout(() => {  disp(x) }, 1000);
-		    setTimeout(() => {
-		    	document.getElementById('bodycontent').style.marginLeft = "-30%";
-			    document.getElementsByClassName('overlape')[0].style.marginLeft = "-30%";
-			    document.getElementsByClassName('col2')[0].style.width = "55%";
-			    document.getElementById('footer').style.marginLeft = "-30%";
-			    document.getElementById('footercontent').style.marginLeft = "30%";
-		    }, 500);
+	var loginbtn = document.getElementById("show_login");
+	loginbtn.addEventListener("click", () => {
+		var x = document.getElementById('content');
+		if(x.className === 'hidden') {
+			document.getElementById('bodycontent').style.marginLeft = "0%";
+			document.getElementsByClassName('overlape')[0].style.marginLeft = "0%";
+			document.getElementsByClassName('col2')[0].style.width = "85%";
+			document.getElementById('footer').style.marginLeft = "0%";
+			document.getElementById('footercontent').style.marginLeft = "0%";
+			x.className = '';
+			disp(x);
+		}else {
+		x.className = 'hidden';
+			setTimeout(() => {  disp(x) }, 1000);
+			setTimeout(() => {
+				document.getElementById('bodycontent').style.marginLeft = "-30%";
+				document.getElementsByClassName('overlape')[0].style.marginLeft = "-30%";
+				document.getElementsByClassName('col2')[0].style.width = "55%";
+				document.getElementById('footer').style.marginLeft = "-30%";
+				document.getElementById('footercontent').style.marginLeft = "30%";
+		}, 500);
 		}
 	});
 }
 function disp(x){
-	if (x.style.height != "0px") {
+	if(x.style.height != "0px") {
 		x.style.height = "0px";
 	}else{
 		x.style.height = "auto";
