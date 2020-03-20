b57f59d290a5c5e4a37e67c6fa9f2202b57c5292
Jaymin <201701203@daiict.ac.in>
2020-03-20 21:53:26 +0530

    unittest for imp function#6

--- app_test.py
+++ app_test.py
@@ -2,7 +2,7 @@ import os
 import unittest
 
 from flask_sqlalchemy import orm, sqlalchemy
-
+from flask import json
 from flaskapp import app, db, models
 
 sessionmaker = orm.sessionmaker
@@ -73,18 +73,18 @@ class impfunction(unittest.TestCase):
         imp_dict = dict(imp=[1, 2], notimp=[3])
         d = json.dumps(imp_dict)
         response1 = self.app.post("/question/new",
-                                  data=dict(question="Is it okay?", mark="8", difficulty=10,
-                                  imp=False, submit="submit"),
+                                  data=dict(question="Is it okay?", mark="8",
+                                   difficulty=10, imp=False, submit="submit"),
                                   follow_redirects=True)
         response2 = self.app.post("/question/new",
-                                  data=dict(question="Is it good?", mark="10", difficulty=20,
-                                  imp=False, submit="submit"),
+                                  data=dict(question="Is it good?", mark="10",
+                                   difficulty=20, imp=False, submit="submit"),
                                   follow_redirects=True)
         response3 = self.app.post("/question/new",
-                                  data=dict(question="knight?", mark="9", difficulty=11,
-                                  imp=True, submit="submit"),
+                                  data=dict(question="knight?", mark="9",
+                                   difficulty=11, imp=True, submit="submit"),
                                   follow_redirects=True)
-        response = self.app.get('/question/imp/%s'%d, follow_redirects=True)
+        response = self.app.get("/question/imp/{0}".format(d), follow_redirects=True)
         self.assertEqual(response1.status_code, 200)
         self.assertEqual(response2.status_code, 200)
         self.assertEqual(response3.status_code, 200)
