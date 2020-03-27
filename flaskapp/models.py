2a2b0adbd084f5e7a768081b49ecb3473adf14df
NIK-1999 <kambariyaniks9999@gmail.com>
2020-03-27 15:42:19 +0530

    Update models.py

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -17,21 +17,3 @@ class Question(db.Model):
                     mark=self.mark,
                     difficulty=self.difficulty,
                     imp=self.imp)
-
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     question = db.Column(db.Text, nullable=False)
-#     mark = db.Column(db.Integer, nullable=False)
-#     difficulty = db.Column(db.Integer, nullable=False)
-#     imp = db.Column(db.Boolean, default=False)
-
-#     def __repr__(self):
-#         return f"Question({self.question}, {self.mark}, {self.difficulty}, {self.imp})"
-
-#     def to_dict(self):
-#         return dict(id=self.id,
-#                     question=self.question,
-#                     mark=self.mark,
-#                     difficulty=self.difficulty,
-#                     imp=self.imp)
