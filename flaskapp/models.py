b6ac9f0a499c1c3bffecd799d9dce335e0a54204
Vaibhav <vaibhavdaiict@gmail.com>
2020-03-28 13:11:46 +0530

    Added MCQ question with diffrent forms

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -17,3 +17,15 @@ class Question(db.Model):
                     mark=self.mark,
                     difficulty=self.difficulty,
                     imp=self.imp)
+                    
+class mcqQuestion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    mark = db.Column(db.Integer, nullable=False)
+    difficulty = db.Column(db.Integer, nullable=False)
+    imp = db.Column(db.Boolean, default=False)
+    option1 = db.Column(db.Text, nullable=False)
+    option2 = db.Column(db.Text, nullable=False)
+    option3 = db.Column(db.Text, nullable=False)
+    option4 = db.Column(db.Text, nullable=False)
+    is_mcq = db.Column(db.Boolean, default=True)
