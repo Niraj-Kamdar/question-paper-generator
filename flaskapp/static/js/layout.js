84bdf0a436ecae778d4d0de2c7bd7d6ff5e431a5
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-04-05 21:49:00 +0530

    bug fix

--- flaskapp/static/js/layout.js
+++ flaskapp/static/js/layout.js
@@ -54,22 +54,3 @@ if (explore_items.length) {
     mainContainer.style.paddingTop = "100px";
   }
 }
-
-$(document).ready(function () {
-  $(".dropdown").hover(
-    function () {
-      $(".dropdown-menu", this)
-        .not(".in .dropdown-menu")
-        .stop(true, true)
-        .slideDown("fast");
-      $(this).toggleClass("open");
-    },
-    function () {
-      $(".dropdown-menu", this)
-        .not(".in .dropdown-menu")
-        .stop(true, true)
-        .slideUp("fast");
-      $(this).toggleClass("open");
-    }
-  );
-});
