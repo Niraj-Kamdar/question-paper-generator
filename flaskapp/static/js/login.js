a0b4fb29821373c7f5d9d9ab59acccfac90b8930
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-04-05 15:51:45 +0530

    bug fix

--- flaskapp/static/js/login.js
+++ flaskapp/static/js/login.js
@@ -7,21 +7,21 @@ const emailError = document.getElementById("form_email_error");
 const passwordError = document.getElementById("form_password_error");
 
 /* form validation */
-loginForm.addEventListener("submit", e => {
-    emailError.innerHTML = "";
-    passwordError.innerHTML = "";
-    const email = emailField.value.trim();
-    const password = passwordField.value.trim();
-    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
-    if (!emailRegex.test(email) || !password) {
-        /*if email is not valid email address */
-        if (!emailRegex.test(email)) {
-            emailError.innerHTML = "enter valid email";
-        }
-        /*if password is empty */
-        if (!password) {
-            passwordError.innerHTML = "password Required!!";
-        }
-        e.preventDefault();
+loginForm.addEventListener("submit", (e) => {
+  emailError.innerHTML = "";
+  passwordError.innerHTML = "";
+  const email = emailField.value.trim();
+  const password = passwordField.value.trim();
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+  if (!emailRegex.test(email) || !password) {
+    /*if email is not valid email address */
+    if (!emailRegex.test(email)) {
+      emailError.innerHTML = "enter valid email";
     }
+    /*if password is empty */
+    if (!password) {
+      passwordError.innerHTML = "password Required!!";
+    }
+    e.preventDefault();
+  }
 });
