67248ca48dda7316506f4265f60832351ac9a0fe
Niraj Kamdar <nirajkamdar27@pm.me>
2020-04-05 19:47:38 +0530

    fix config

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -6,7 +6,7 @@ class Config:
     TESTING = False
     WTF_CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
