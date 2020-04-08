477578289ff888c8874cf4820788ea04a2e10be5
Karpit <karpitpatel786@gmail.com>
2020-04-08 12:25:14 +0530

    Update test_profile_picture.py

--- test/users/test_profile_picture.py
+++ test/users/test_profile_picture.py
@@ -1,9 +1,4 @@
-import re
-
-from bs4 import BeautifulSoup
 from PIL import Image
-import requests
-
 from flaskapp import models
 from test.main.base_classes import BaseUser
 from test.main.utils import test_post_request
@@ -13,17 +8,20 @@ class UserAccountTestCase(BaseUser):
     def test_set_profile(self):
         self.login()
         im1 = Image.open("profile1.png")
-        set_user = dict(username="nr.nutron", email="nutron@gmail.com", picture=im1, submit="submit")
+        set_user = dict(username="nr.nutron", email="nutron@gmail.com",
+                         picture=im1, submit="submit")
         test_post_request(self, "/account", set_user, models.User, 1)
 
     def test_update_profile(self):
         self.login()
         # first set profile
         im1 = Image.open("profile1.png")
-        set_user = dict(username="nr.nutron", email="nutron@gmail.com", picture=im1, submit="submit")
+        set_user = dict(username="nr.nutron", email="nutron@gmail.com",
+                         picture=im1, submit="submit")
         test_post_request(self, "/account", set_user, models.User, 1)
 
         # update with new
         im2 = Image.open("profile2.png")
-        set_user = dict(username="nr.nutron", email="nutron@gmail.com", picture=im2, submit="submit")
+        set_user = dict(username="nr.nutron", email="nutron@gmail.com",
+                         picture=im2, submit="submit")
         test_post_request(self, "/account", set_user, models.User, 1)
