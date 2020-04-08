cc4954caaa3c58ad03fe7efa47b6267d151fc158
Karpit <karpitpatel786@gmail.com>
2020-04-08 16:43:14 +0530

    Update test_profile_picture.py

--- test/users/test_profile_picture.py
+++ test/users/test_profile_picture.py
@@ -1,12 +1,9 @@
 import os
 from test.main.base_classes import BaseUser
-from test import TEST_PATH
-
 
 class UserAccountTestCase(BaseUser):
     def test_profile(self):
         self.login()
         url = '/account'
-        im1 = os.path.join(TEST_PATH, "users", "profile1.png")
-        files = {'picture': open(im1, 'rb')}
+        files = {'picture': open('profile1.png', 'rb')}
         self.client.post(url, files=files)
