5f447eb2562003a544e04d49bbe397ceba6056c7
Karpit <karpitpatel786@gmail.com>
2020-04-08 15:09:01 +0530

    profile picture

--- test/users/test_profile_picture.py
+++ test/users/test_profile_picture.py
@@ -1,27 +1,15 @@
 from PIL import Image
+import os
+import requests
 from flaskapp import models
 from test.main.base_classes import BaseUser
-from test.main.utils import test_post_request
+from test import TEST_PATH
 
 
 class UserAccountTestCase(BaseUser):
-    def test_set_profile(self):
+    def test_profile(self):
         self.login()
-        im1 = Image.open("profile1.png")
-        set_user = dict(username="nr.nutron", email="nutron@gmail.com",
-                         picture=im1, submit="submit")
-        test_post_request(self, "/account", set_user, models.User, 1)
-
-    def test_update_profile(self):
-        self.login()
-        # first set profile
-        im1 = Image.open("profile1.png")
-        set_user = dict(username="nr.nutron", email="nutron@gmail.com",
-                         picture=im1, submit="submit")
-        test_post_request(self, "/account", set_user, models.User, 1)
-
-        # update with new
-        im2 = Image.open("profile2.png")
-        set_user = dict(username="nr.nutron", email="nutron@gmail.com",
-                         picture=im2, submit="submit")
-        test_post_request(self, "/account", set_user, models.User, 1)
+        url = '/account'
+        im1 = os.path.join(TEST_PATH, "users", "profile1.png")
+        im = Image.open(im1)
+        requests.post(url, picture=im)
