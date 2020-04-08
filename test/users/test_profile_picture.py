bed6dfa444b0b1f76f486c67765ffa8faa95624d
Karpit <karpitpatel786@gmail.com>
2020-04-08 17:40:45 +0530

    profile picture

--- test/users/test_profile_picture.py
+++
@@ -1,9 +0,0 @@
-from test.main.base_classes import BaseUser
-
-
-class UserAccountTestCase(BaseUser):
-    def test_profile(self):
-        self.login()
-        url = '/account'
-        files = {'picture': open('profile1.png', 'rb')}
-        self.client.post(url, files=files)
