959a0c19dc7ecfcdd7399a3ddefa2753ab27e614
Niraj Kamdar <nirajkamdar27@pm.me>
2020-04-10 01:08:27 +0530

    reformat

--- flaskapp/static/js/home.js
+++ flaskapp/static/js/home.js
@@ -1,9 +1,9 @@
 (function () {
-  document.getElementById("template_display").style.width = "100%";
-  window.onload = function () {
-    var manage_courses_btn = document.getElementById("manage_courses_btn");
-    manage_courses_btn.addEventListener("click", () => {
-      window.location.href = "/course";
-    });
-  };
+    document.getElementById("template_display").style.width = "100%";
+    window.onload = function () {
+        var manage_courses_btn = document.getElementById("manage_courses_btn");
+        manage_courses_btn.addEventListener("click", () => {
+            window.location.href = "/course";
+        });
+    };
 })();
