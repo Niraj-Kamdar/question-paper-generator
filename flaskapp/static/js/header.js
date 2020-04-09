ad35fd9b8880ac33dabe4fe2df45a63c4098dec4
jeel2308 <42006763+jeel2308@users.noreply.github.com>
2020-04-09 20:32:56 +0530

    bug fix

--- flaskapp/static/js/header.js
+++ flaskapp/static/js/header.js
@@ -1,21 +1,9 @@
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  var header = document.getElementById("header");
-  var ofset = 50;
-  if (
-    window.pageYOffset > ofset ||
-    document.body.scrollTop > ofset ||
-    document.documentElement.scrollTop > ofset
-  ) {
-    header.classList.add("py-2");
-    header.classList.remove("py-4");
-    header.style.background = "#9000F0";
-  } else {
-    header.classList.add("py-4");
-    header.classList.remove("py-2");
-    header.style.background = "";
-  }
-}
+(function () {
+  document.getElementById("logo").addEventListener("click", () => {
+    const url = window.location.href;
+    let index = url.indexOf("/", 0);
+    index = url.indexOf("/", index + 1);
+    index = url.indexOf("/", index + 1);
+    window.location.href = window.location.href.substr(0, index + 1);
+  });
+})();
