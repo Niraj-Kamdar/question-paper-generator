61009e6fd4c91fc2f320f06e2b12e8e9a88965b3
Niraj Kamdar <nirajkamdar27@pm.me>
2020-04-16 02:08:53 +0530

    Revert "expr"
    This reverts commit 15aca3f6636390b9e18d40d72f39ce01243cde0a.

--- flaskapp/blueprints/users/forms.py
+++ flaskapp/blueprints/users/forms.py
@@ -1,3 +1,4 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, FileField, BooleanField
@@ -6,42 +7,46 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from flaskapp.models import User
 
 
-def validate_email_exists(form, email):
-    user = User.query.filter_by(email=email.data).first()
-    if user is None:
-        raise ValidationError('There is no account with that email. You must register first.')
-
-
-def validate_username(form, username):
-    user = User.query.filter_by(username=username.data).first()
-    if user:
-        raise ValidationError('That username is taken. Please choose a different one.')
-
-
-def validate_email(form, email):
-    user = User.query.filter_by(email=email.data).first()
-    if user:
-        raise ValidationError('That email is taken. Please choose a different one.')
-
-
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20), validate_username])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email(), validate_email])
+                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20), validate_username])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email(), validate_email])
+                        validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -54,8 +59,13 @@ class LoginForm(FlaskForm):
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
-                        validators=[DataRequired(), Email(), validate_email_exists])
+                        validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
 
 
 class ResetPasswordForm(FlaskForm):
