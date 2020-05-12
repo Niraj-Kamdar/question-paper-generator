4ae4974f0f50f4fdf5ed096f99fd12d741c51bf9
Restyled.io <commits@restyled.io>
2020-05-12 16:15:36 +0000

    Restyled by prettier

--- flaskapp/static/js/.eslintrc.js
+++ flaskapp/static/js/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "env" : {"browser" : true, "commonjs" : true, "es6" : true},
-  "extends" : "eslint:recommended",
-  "globals" : {"Atomics" : "readonly", "SharedArrayBuffer" : "readonly"},
-  "parserOptions" : {"ecmaVersion" : 2018},
-  "rules" : {}
+  env: { browser: true, commonjs: true, es6: true },
+  extends: "eslint:recommended",
+  globals: { Atomics: "readonly", SharedArrayBuffer: "readonly" },
+  parserOptions: { ecmaVersion: 2018 },
+  rules: {},
 };
