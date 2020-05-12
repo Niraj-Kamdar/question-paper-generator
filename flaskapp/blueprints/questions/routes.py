a2865dba3ac42e64778d4ab008fba0fe20055eeb
Restyled.io <commits@restyled.io>
2020-05-12 11:26:59 +0000

    Restyled by isort

--- flaskapp/blueprints/questions/routes.py
+++ flaskapp/blueprints/questions/routes.py
@@ -1,21 +1,14 @@
-from flask import Blueprint, flash, json, redirect, render_template, request, url_for
+from flask import (Blueprint, flash, json, redirect, render_template, request,
+                   url_for)
 from flask_login import current_user, login_required
-
 from flaskapp import db
-from flaskapp.blueprints.questions.forms import (
-    CourseForm,
-    MCQQuestionForm,
-    QuestionForm,
-    UnitForm,
-)
-from flaskapp.blueprints.questions.utils import (
-    check_valid_course,
-    check_valid_question_type,
-    check_valid_unit,
-    update_imp,
-)
+from flaskapp.blueprints.questions.forms import (CourseForm, MCQQuestionForm,
+                                                 QuestionForm, UnitForm)
+from flaskapp.blueprints.questions.utils import (check_valid_course,
+                                                 check_valid_question_type,
+                                                 check_valid_unit, update_imp)
 from flaskapp.models import Course, MCQQuestion, Question, Unit
-from flaskapp.utils import profile_path, DifficultyLevel, CognitiveLevel
+from flaskapp.utils import CognitiveLevel, DifficultyLevel, profile_path
 
 questions = Blueprint("questions", __name__)
 
