e932020d97aebd6883892888a6e1bab03d7f21c2
Niraj Kamdar <nirajkamdar27@pm.me>
2020-06-11 22:38:09 +0530

    paper form is working

---
+++ test/papers/test_paper_generate_request.py
@@ -0,0 +1,20 @@
+from flaskapp.utils import json_url
+from test.main.base_classes import BaseUnit
+from test.main.utils import test_post_request
+
+
+class PaperGenerateRequest(BaseUnit):
+    def test_paper_generate_request(self):
+        data = dict(questions=[1, 2, 3], total_marks=10)
+        response, _ = test_post_request(self, "/course/1/papers/generate/request", data)
+        # FIXME: assert something here: response.data
+
+    def test_mark_distribution_form(self):
+        questions = [1, 2, 3]
+        data = {'Unit:01': '30', 'Knowledge': '10', 'Comprehension': '10', 'Application': '10', 'Easy': '10',
+                'Medium': '10', 'Hard': '10', 'Que.1.A': '5', 'Que.2.A': '5', 'Que.2.B': '5', 'Que.3.A': '5',
+                'Que.3.B': '5', 'Que.3.C': '5'}
+
+        token = json_url.dumps(dict(questions=questions, total_marks=30))
+        response, _ = test_post_request(self, f"/course/1/papers/generate/form/{token}", data)
+        # FIXME: do some assertion here
