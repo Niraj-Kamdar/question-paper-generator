3cfa6b64e5282c679f14994a365649c6c1e39b5f
jeel2308 <jeelpatel238@gmail.com>
2020-06-14 16:22:54 +0530

    code refactored

--- flaskapp/blueprints/papers/routes.py
+++ flaskapp/blueprints/papers/routes.py
@@ -5,7 +5,6 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
-from flask_login import current_user
 from flask_login import login_required
 
 from flaskapp.blueprints.papers.forms import MarkDistributionForm
