aef197ad6c50eb8c4236f0b88b1daccae4141cf6
Niraj Kamdar <51387861+Niraj-Kamdar@users.noreply.github.com>
2020-06-15 23:08:16 +0530

    Update test_contact_us.py

--- test/main/test_contact_us.py
+++ test/main/test_contact_us.py
@@ -3,7 +3,7 @@ from test.main.base_classes import BaseUser
 from test.main.utils import test_post_request
 
 
-class WebpageTestCase(BaseUser):
+class ContactUsTestCase(BaseUser):
     def test_contact_us(self):
         with self.mail.record_messages() as outbox:
             data = dict(
