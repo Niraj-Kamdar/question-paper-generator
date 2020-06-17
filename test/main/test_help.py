7a2eaaf640debbf60a12ecac5bd2f0ae36f36688
Jaymin <201701203@daiict.ac.in>
2020-06-17 13:24:01 +0530

    unit test - help, policy, terms

--- test/main/test_help.py
+++ test/main/test_help.py
@@ -6,14 +6,12 @@ from test.main.utils import test_post_request
 class HelpTestCase(BaseUser):
     def test_help(self):
         response = self.client.get("/help")
-        self.assertIn(b"Below is some frequently asked questions which may help you.", response.data)
+        self.assertIn(b"Below is some frequently asked questions which", response.data)
         self.assertIn(b"We're dedicated to giving you the very best of our service.", response.data)
-        self.assertIn(b"If you have any other questions or comments, please don't hesitate to contact us.", response.data)
-        self.assertIn(b"Yes, you have to login to our system to use services provided by us.", response.data)
-        self.assertIn(b"You can login/signup by email address and set password for your account.", response.data)
+        self.assertIn(b"If you have any other questions or comments.", response.data)
+        self.assertIn(b"Yes, you have to login to our system to use", response.data)
+        self.assertIn(b"You can login/signup by email address and set", response.data)
         self.assertIn(b"If you forget password then you can set new password.", response.data)
-        self.assertIn(b"For setting new password click on forget password, you will get link for set new password if you can’t find in inbox then check spam folder.", response.data)
-        self.assertIn(b"Maybe there are not enough question to meet your requirement (total marks or difficulty level)", response.data)
-        self.assertIn(b"Maybe Too much questions are marked as ‘always include in papers’ so that total marks of", response.data)
-        self.assertIn(b"marked questions will be more than required marks of question paper.", response.data)
-
+        self.assertIn(b"Maybe there are not enough question to meet your requirement", response.data)
+        self.assertIn(b"Maybe Too much questions are marked as ‘always include", response.data)
+        self.assertIn(b"marked questions will be more than required", response.data)
