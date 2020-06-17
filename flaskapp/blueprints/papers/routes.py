06031cfef26b72f4d0f8ca0ac3cea5da27fe4192
Restyled.io <commits@restyled.io>
2020-06-17 14:24:51 +0000

    Restyled by reorder-python-imports

--- flaskapp/blueprints/papers/routes.py
+++ flaskapp/blueprints/papers/routes.py
@@ -1,12 +1,19 @@
 import itertools
 
-from flask import (Blueprint, flash, jsonify, redirect, render_template,
-                   request, url_for)
+from flask import Blueprint
+from flask import flash
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
 from flask_login import login_required
+from qpt_generator import QPTGenerator
+
 from flaskapp.blueprints.papers.forms import MarkDistributionForm
 from flaskapp.checkers import check_valid_course
-from flaskapp.utils import json_url, profile_path
-from qpt_generator import QPTGenerator
+from flaskapp.utils import json_url
+from flaskapp.utils import profile_path
 
 papers = Blueprint("papers", __name__)
 
