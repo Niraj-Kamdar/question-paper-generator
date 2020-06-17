b96433ee1a71b2f42649708c7eb17472c9e789a4
Restyled.io <commits@restyled.io>
2020-06-17 08:04:15 +0000

    Restyled by autopep8

--- test/main/test_help.py
+++ test/main/test_help.py
@@ -6,11 +6,18 @@ from test.main.utils import test_post_request
 class HelpTestCase(BaseUser):
     def test_help(self):
         response = self.client.get("/help")
-        self.assertIn(b"Below is some frequently asked questions which", response.data)
-        self.assertIn(b"dedicated to giving you the very best of our service.", response.data)
-        self.assertIn(b"Yes, you have to login to our system to use", response.data)
-        self.assertIn(b"You can login/signup by email address and set", response.data)
-        self.assertIn(b"If you forget password then you can set new password.", response.data)
-        self.assertIn(b"Maybe there are not enough question to meet your requirement", response.data)
+        self.assertIn(
+            b"Below is some frequently asked questions which", response.data)
+        self.assertIn(
+            b"dedicated to giving you the very best of our service.", response.data)
+        self.assertIn(
+            b"Yes, you have to login to our system to use", response.data)
+        self.assertIn(
+            b"You can login/signup by email address and set", response.data)
+        self.assertIn(
+            b"If you forget password then you can set new password.", response.data)
+        self.assertIn(
+            b"Maybe there are not enough question to meet your requirement", response.data)
         self.assertIn(b"Maybe Too much questions are marked as", response.data)
-        self.assertIn(b"marked questions will be more than required", response.data)
+        self.assertIn(
+            b"marked questions will be more than required", response.data)
--- test/main/test_terms_of_services.py
+++ test/main/test_terms_of_services.py
@@ -6,27 +6,49 @@ from test.main.utils import test_post_request
 class TermsOfServiceTestCase(BaseUser):
     def test_terms_of_service(self):
         response = self.client.get("/terms-of-service")
-        self.assertIn(b"you are agreeing to be bound by these terms of", response.data)
-        self.assertIn(b"responsible for compliance with any applicable local laws.", response.data)
-        self.assertIn(b"The materials contained in this website are protected", response.data)
-        self.assertIn(b"by applicable copyright and trademark law.", response.data)
-        self.assertIn(b"Permission is hereby granted, free of charge,", response.data)
+        self.assertIn(
+            b"you are agreeing to be bound by these terms of", response.data)
+        self.assertIn(
+            b"responsible for compliance with any applicable local laws.", response.data)
+        self.assertIn(
+            b"The materials contained in this website are protected", response.data)
+        self.assertIn(
+            b"by applicable copyright and trademark law.", response.data)
+        self.assertIn(
+            b"Permission is hereby granted, free of charge,", response.data)
         self.assertIn(b"a copy of this software and associated", response.data)
-        self.assertIn(b"The above copyright notice and this permission", response.data)
-        self.assertIn(b"be included in all copies or substantial portion", response.data)
-        self.assertIn(b"This license shall automatically terminate if you", response.data)
-        self.assertIn(b"any of these restrictions and may be terminated by", response.data)
-        self.assertIn(b"all other warranties including, without limitation,", response.data)
-        self.assertIn(b"warranties or conditions of merchantability, fitnes", response.data)
-        self.assertIn(b"or non-infringement of intellectual property or other", response.data)
-        self.assertIn(b"of the materials on its website or otherwise", response.data)
-        self.assertIn(b"no event shall SetNow or its suppliers be", response.data)
-        self.assertIn(b"including, without limitation, damages for loss of data", response.data)
-        self.assertIn(b"However SetNow does not make any commitment to", response.data)
-        self.assertIn(b"SetNow has not reviewed all of the sites linked to its", response.data)
-        self.assertIn(b"linked site. The inclusion of any link does not imply", response.data)
-        self.assertIn(b"SetNow may revise these terms of service for its website", response.data)
-        self.assertIn(b"By using this website you are agreeing to be bound by the then", response.data)
-        self.assertIn(b"These terms and conditions are governed by and construed in", response.data)
-        self.assertIn(b"and you irrevocably submit to the exclusive jurisdiction of", response.data)
+        self.assertIn(
+            b"The above copyright notice and this permission", response.data)
+        self.assertIn(
+            b"be included in all copies or substantial portion", response.data)
+        self.assertIn(
+            b"This license shall automatically terminate if you", response.data)
+        self.assertIn(
+            b"any of these restrictions and may be terminated by", response.data)
+        self.assertIn(
+            b"all other warranties including, without limitation,", response.data)
+        self.assertIn(
+            b"warranties or conditions of merchantability, fitnes", response.data)
+        self.assertIn(
+            b"or non-infringement of intellectual property or other", response.data)
+        self.assertIn(
+            b"of the materials on its website or otherwise", response.data)
+        self.assertIn(
+            b"no event shall SetNow or its suppliers be", response.data)
+        self.assertIn(
+            b"including, without limitation, damages for loss of data", response.data)
+        self.assertIn(
+            b"However SetNow does not make any commitment to", response.data)
+        self.assertIn(
+            b"SetNow has not reviewed all of the sites linked to its", response.data)
+        self.assertIn(
+            b"linked site. The inclusion of any link does not imply", response.data)
+        self.assertIn(
+            b"SetNow may revise these terms of service for its website", response.data)
+        self.assertIn(
+            b"By using this website you are agreeing to be bound by the then", response.data)
+        self.assertIn(
+            b"These terms and conditions are governed by and construed in", response.data)
+        self.assertIn(
+            b"and you irrevocably submit to the exclusive jurisdiction of", response.data)
         self.assertIn(b"Terms Of Service", response.data)
