d508c86ec9da6fc766bdc241dc78075adfad980a
Niraj Kamdar <nirajkamdar27@pm.me>
2020-06-19 13:52:21 +0530

    fix bugs

--- test/papers/test_paper_generate_request.py
+++ test/papers/test_paper_generate_request.py
@@ -1,6 +1,5 @@
 from flask import json
 
-from flaskapp.utils import json_url
 from test.main.base_classes import BaseUnit
 from test.main.utils import test_post_request
 
@@ -13,7 +12,7 @@ class PaperGenerateRequest(BaseUnit):
             data=json.dumps(data),
             headers={"Content-Type": "application/json"},
         )
-        print(response.data.decode())
+        # print(response.data.decode())
         # FIXME: assert something here: response.data
 
     def test_mark_distribution_form(self):
@@ -38,12 +37,11 @@ class PaperGenerateRequest(BaseUnit):
         response, _ = test_post_request(self,
                                         f"/course/1/papers/generate/form/",
                                         data)
-        print(response.data.decode().split("title")[1])
+        # print(response.data.decode().split("title")[1])
         response = self.client.post(
             "/course/1/papers/confirm/template/",
             data=json.dumps(dict(status="OK")),
             headers={"Content-Type": "application/json"},
         )
-        print(response.data)
+        # print(response.data)
         # FIXME: do some assertion here
-        # {'Unit:01': '30', 'Knowledge': '10', 'Comprehension': '10', 'Application': '10', 'Easy': '10', 'Medium': '10', 'Hard': '10', 'Que.1.A': '5', 'Que.2.A': '5', 'Que.2.B': '5', 'Que.3.A': '5', 'Que.3.B': '5', 'Que.3.C': '5', 'sub': '15', 'mcq': '15'}
