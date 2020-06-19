947ee533de6846f633cbc7dc72d63c8401adbe3c
Restyled.io <commits@restyled.io>
2020-06-19 09:26:29 +0000

    Restyled by isort

--- test/papers/test_paper_generate_request.py
+++ test/papers/test_paper_generate_request.py
@@ -1,7 +1,7 @@
-from flask import json
-
 from test.main.base_classes import BaseUnit
 from test.main.utils import test_post_request
+
+from flask import json
 
 
 class PaperGenerateRequest(BaseUnit):
