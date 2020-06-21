6359c3aef6d7526ab6681a74022b127e5b23e3ac
Restyled.io <commits@restyled.io>
2020-06-21 07:08:47 +0000

    Restyled by black

--- flaskapp/blueprints/questions/forms.py
+++ flaskapp/blueprints/questions/forms.py
@@ -11,15 +11,10 @@ from wtforms.validators import NumberRange
 
 
 class QuestionForm(FlaskForm):
-    question = TextAreaField("Question",
-                             validators=[DataRequired(),
-                                         Length(min=2)])
+    question = TextAreaField("Question", validators=[DataRequired(), Length(min=2)])
     mark = IntegerField(
         "Mark",
-        validators=[
-            DataRequired(),
-            NumberRange(1, 101, "Not in a valid mark range")
-        ],
+        validators=[DataRequired(), NumberRange(1, 101, "Not in a valid mark range")],
     )
     difficulty = SelectField(
         "Difficulty Level",
