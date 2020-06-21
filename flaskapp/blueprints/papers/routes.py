f2449696ac98feb5b833e2f211ea924d99644a31
Restyled.io <commits@restyled.io>
2020-06-21 07:09:07 +0000

    Restyled by yapf

--- flaskapp/blueprints/papers/routes.py
+++ flaskapp/blueprints/papers/routes.py
@@ -32,7 +32,8 @@ def home():
     )
 
 
-@papers.route("/course/<course_id>/papers/generate/request", methods=["GET", "POST"])
+@papers.route("/course/<course_id>/papers/generate/request",
+              methods=["GET", "POST"])
 @login_required
 @check_valid_course
 def paper_generate_request(course_id):
@@ -43,8 +44,9 @@ def paper_generate_request(course_id):
         if data:
             data = json_url.dumps(data)
             return redirect(
-                url_for("papers.mark_distribution_form", course_id=course_id, data=data)
-            )
+                url_for("papers.mark_distribution_form",
+                        course_id=course_id,
+                        data=data))
         flash("Form can't be empty!")
     return render_template(
         "papers/generate_request.html",
@@ -55,16 +57,17 @@ def paper_generate_request(course_id):
     )
 
 
-@papers.route(
-    "/course/<course_id>/papers/generate/form/<data>", methods=["GET", "POST"]
-)
+@papers.route("/course/<course_id>/papers/generate/form/<data>",
+              methods=["GET", "POST"])
 @login_required
 @check_valid_course
 def mark_distribution_form(course_id, data):
     if not data:
-        return redirect(url_for("papers.paper_generate_request", course_id=course_id))
+        return redirect(
+            url_for("papers.paper_generate_request", course_id=course_id))
     data = json_url.loads(data)
-    form = MarkDistributionForm(course_id, data["questions"], data["total_marks"])
+    form = MarkDistributionForm(course_id, data["questions"],
+                                data["total_marks"])
     if form.validate_on_submit():
         return jsonify(form.data)
     return render_template("papers/mark_distribution_form.html", form=form)
@@ -97,8 +100,10 @@ def ptp():
 
     s_questions = {
         1: {
-            "question": "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
-            "marks": 10,
+            "question":
+            "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
+            "marks":
+            10,
             "sub_questions": [
                 "Heating at constant volume until the pressure is 208 kPa",
                 "Polytropic compression to 0.006 m3, where n = 1.6",
@@ -107,8 +112,10 @@ def ptp():
             ],
         },
         2: {
-            "question": "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
-            "marks": 15,
+            "question":
+            "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
+            "marks":
+            15,
             "sub_questions": [
                 "Heating at constant volume until the pressure is 208 kPa",
                 "Polytropic compression to 0.006 m3, where n = 1.6",
@@ -117,8 +124,10 @@ def ptp():
             ],
         },
         3: {
-            "question": "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
-            "marks": 18,
+            "question":
+            "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
+            "marks":
+            18,
             "sub_questions": [
                 "Heating at constant volume until the pressure is 208 kPa",
                 "Polytropic compression to 0.006 m3, where n = 1.6",
@@ -127,8 +136,10 @@ def ptp():
             ],
         },
         4: {
-            "question": "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
-            "marks": 10,
+            "question":
+            "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
+            "marks":
+            10,
             "sub_questions": [
                 "Heating at constant volume until the pressure is 208 kPa",
                 "Polytropic compression to 0.006 m3, where n = 1.6",
@@ -137,8 +148,10 @@ def ptp():
             ],
         },
         5: {
-            "question": "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
-            "marks": 15,
+            "question":
+            "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
+            "marks":
+            15,
             "sub_questions": [
                 "Heating at constant volume until the pressure is 208 kPa",
                 "Polytropic compression to 0.006 m3, where n = 1.6",
@@ -147,8 +160,10 @@ def ptp():
             ],
         },
         6: {
-            "question": "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
-            "marks": 18,
+            "question":
+            "A quantity of air has a pressure, temprature and volume of 104 kPa, 38 °C and 0.03 m3, respectively. The tempreture of the air is raised by following to ways, for each of the above cases calculate the final tempreture, work requirement, change in internal energy and heat requirement.",
+            "marks":
+            18,
             "sub_questions": [
                 "Heating at constant volume until the pressure is 208 kPa",
                 "Polytropic compression to 0.006 m3, where n = 1.6",
--- test/main/base_classes.py
+++ test/main/base_classes.py
@@ -94,9 +94,8 @@ class BaseSubQuestion(BaseUnit):
             imp=None,
             submit="submit",
         )
-        test_post_request(
-            self, "/course/1/unit/1/question/sub/new/", new_question, models.Question, 1
-        )
+        test_post_request(self, "/course/1/unit/1/question/sub/new/",
+                          new_question, models.Question, 1)
 
         new_question = dict(
             question="Is it question?",
@@ -106,9 +105,8 @@ class BaseSubQuestion(BaseUnit):
             imp=True,
             submit="submit",
         )
-        test_post_request(
-            self, "/course/1/unit/1/question/sub/new/", new_question, models.Question, 2
-        )
+        test_post_request(self, "/course/1/unit/1/question/sub/new/",
+                          new_question, models.Question, 2)
 
         new_question = dict(
             question="What is it?",
@@ -118,9 +116,8 @@ class BaseSubQuestion(BaseUnit):
             imp=None,
             submit="submit",
         )
-        test_post_request(
-            self, "/course/1/unit/1/question/sub/new/", new_question, models.Question, 3
-        )
+        test_post_request(self, "/course/1/unit/1/question/sub/new/",
+                          new_question, models.Question, 3)
 
         new_question = dict(
             question="What was that?",
@@ -130,9 +127,8 @@ class BaseSubQuestion(BaseUnit):
             imp=None,
             submit="submit",
         )
-        test_post_request(
-            self, "/course/1/unit/1/question/sub/new/", new_question, models.Question, 4
-        )
+        test_post_request(self, "/course/1/unit/1/question/sub/new/",
+                          new_question, models.Question, 4)
 
         new_question = dict(
             question="How are you?",
@@ -142,9 +138,8 @@ class BaseSubQuestion(BaseUnit):
             imp=True,
             submit="submit",
         )
-        test_post_request(
-            self, "/course/1/unit/1/question/sub/new/", new_question, models.Question, 5
-        )
+        test_post_request(self, "/course/1/unit/1/question/sub/new/",
+                          new_question, models.Question, 5)
 
 
 class BaseMCQQuestion(BaseUnit):
