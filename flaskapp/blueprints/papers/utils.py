a659d87ffd1c2c94d783f8d555e2045a8f624bd3
Restyled.io <commits@restyled.io>
2020-06-21 17:13:52 +0000

    Restyled by isort

--- flaskapp/blueprints/papers/utils.py
+++ flaskapp/blueprints/papers/utils.py
@@ -1,12 +1,11 @@
 import os
 import secrets
 
-from PIL import Image
 from flask import current_app
-from sqlalchemy import and_, func
-
 from flaskapp import db
 from flaskapp.models import Question, Unit
+from PIL import Image
+from sqlalchemy import and_, func
 
 
 def find_conflicting_questions(course_id, constraints):
