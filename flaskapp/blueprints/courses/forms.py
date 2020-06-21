0dc61371234ffdc25513f33efea34a463ff4b6b3
Restyled.io <commits@restyled.io>
2020-06-21 18:01:30 +0000

    Restyled by reorder-python-imports

--- flaskapp/blueprints/courses/forms.py
+++ flaskapp/blueprints/courses/forms.py
@@ -1,10 +1,18 @@
 from flask_login import current_user
 from flask_wtf import FlaskForm
+from sqlalchemy import and_
+from sqlalchemy import func
+from wtforms import BooleanField
+from wtforms import IntegerField
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
+from wtforms.validators import NumberRange
+from wtforms.validators import ValidationError
+
 from flaskapp import db
-from flaskapp.models import Course, Unit
-from sqlalchemy import and_, func
-from wtforms import BooleanField, IntegerField, StringField, SubmitField
-from wtforms.validators import DataRequired, NumberRange, ValidationError
+from flaskapp.models import Course
+from flaskapp.models import Unit
 
 
 def validate_course_name(form, course_name):
