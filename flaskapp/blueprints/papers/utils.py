bf6b1d3a299cc0cd9008f720775adc4422be5953
Niraj Kamdar <nirajkamdar27@pm.me>
2020-06-21 22:40:51 +0530

    fix bug in generate paper

--- flaskapp/blueprints/papers/utils.py
+++ flaskapp/blueprints/papers/utils.py
@@ -1,38 +1,45 @@
 import os
 import secrets
 
-from flask import current_app
 from PIL import Image
-from sqlalchemy import and_
-from sqlalchemy import func
+from flask import current_app
+from sqlalchemy import and_, func
 
 from flaskapp import db
-from flaskapp.models import Question
+from flaskapp.models import Question, Unit
 
 
 def find_conflicting_questions(course_id, constraints):
+    unit = db.session.Query(Unit).filter_by(
+            and_(
+                    Unit.chapter_no == constraints["unit"],
+                    Unit.course_id == course_id
+            )).first()
     return (db.session.Query(Question).filter_by(
-        and_(
-            Question.cognitive_level == constraints["cognitive"],
-            Question.difficulty == constraints["difficulty"],
-            Question.mark == constraints["mark"],
-            Question.unit.chapter_no == constraints["unit"],
-            Question.unit.course_id == course_id,
-            Question.question_type == constraints["question_type"],
-            Question.imp is True,
-            Question.is_asked is True,
-        )).all())
+            and_(
+                    Question.cognitive_level == constraints["cognitive"],
+                    Question.difficulty == constraints["difficulty"],
+                    Question.mark == constraints["mark"],
+                    Question.unit_id == unit.id,
+                    Question.question_type == constraints["question_type"],
+                    Question.imp is True,
+                    Question.is_asked is True,
+            )).all())
 
 
 def find_random_question(course_id, constraints):
+    unit = db.session.Query(Unit).filter_by(
+            and_(
+                    Unit.chapter_no == constraints["unit"],
+                    Unit.course_id == course_id
+            )).first()
     return (db.session.Query(Question).filter_by(
-        and_(
-            Question.cognitive_level == constraints["cognitive"],
-            Question.difficulty == constraints["difficulty"],
-            Question.mark == constraints["mark"],
-            Question.unit.chapter_no == constraints["unit"],
-            Question.unit.course_id == course_id,
-        )).order_by(func.random()).first().id)
+            and_(
+                    Question.cognitive_level == constraints["cognitive"],
+                    Question.difficulty == constraints["difficulty"],
+                    Question.mark == constraints["mark"],
+                    Question.unit_id == unit.id
+            )).order_by(func.random()).first().id)
 
 
 def save_logo(form_picture):
@@ -46,7 +53,8 @@ def save_logo(form_picture):
     """
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = secrets.token_urlsafe(10) + f_ext
-    picture_path = os.path.join(current_app.root_path, "static/logos",
+    picture_path = os.path.join(current_app.root_path,
+                                "static/logos",
                                 picture_fn)
 
     output_size = (400, 400)
