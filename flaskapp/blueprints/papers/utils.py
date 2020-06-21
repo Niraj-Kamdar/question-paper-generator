51e56ff5ae7480221ef8740a7b46cf420b681bb8
Restyled.io <commits@restyled.io>
2020-06-21 17:13:51 +0000

    Restyled by black

--- flaskapp/blueprints/papers/utils.py
+++ flaskapp/blueprints/papers/utils.py
@@ -10,12 +10,16 @@ from flaskapp.models import Question, Unit
 
 
 def find_conflicting_questions(course_id, constraints):
-    unit = db.session.Query(Unit).filter_by(
-        and_(
-            Unit.chapter_no == constraints["unit"],
-            Unit.course_id == course_id
-        )).first()
-    return (db.session.Query(Question).filter_by(
+    unit = (
+        db.session.Query(Unit)
+        .filter_by(
+            and_(Unit.chapter_no == constraints["unit"], Unit.course_id == course_id)
+        )
+        .first()
+    )
+    return (
+        db.session.Query(Question)
+        .filter_by(
             and_(
                 Question.cognitive_level == constraints["cognitive"],
                 Question.difficulty == constraints["difficulty"],
@@ -24,22 +28,34 @@ def find_conflicting_questions(course_id, constraints):
                 Question.question_type == constraints["question_type"],
                 Question.imp is True,
                 Question.is_asked is True,
-            )).all())
+            )
+        )
+        .all()
+    )
 
 
 def find_random_question(course_id, constraints):
-    unit = db.session.Query(Unit).filter_by(
-        and_(
-            Unit.chapter_no == constraints["unit"],
-            Unit.course_id == course_id
-        )).first()
-    return (db.session.Query(Question).filter_by(
+    unit = (
+        db.session.Query(Unit)
+        .filter_by(
+            and_(Unit.chapter_no == constraints["unit"], Unit.course_id == course_id)
+        )
+        .first()
+    )
+    return (
+        db.session.Query(Question)
+        .filter_by(
             and_(
                 Question.cognitive_level == constraints["cognitive"],
                 Question.difficulty == constraints["difficulty"],
                 Question.mark == constraints["mark"],
-                Question.unit_id == unit.id
-            )).order_by(func.random()).first().id)
+                Question.unit_id == unit.id,
+            )
+        )
+        .order_by(func.random())
+        .first()
+        .id
+    )
 
 
 def save_logo(form_picture):
@@ -53,9 +69,7 @@ def save_logo(form_picture):
     """
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = secrets.token_urlsafe(10) + f_ext
-    picture_path = os.path.join(current_app.root_path,
-                                "static/logos",
-                                picture_fn)
+    picture_path = os.path.join(current_app.root_path, "static/logos", picture_fn)
 
     output_size = (400, 400)
     i = Image.open(form_picture)
