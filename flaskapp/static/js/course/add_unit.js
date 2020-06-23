f8e8028c6a91e0d86d4bbc587de963a01ddcde7f
Restyled.io <commits@restyled.io>
2020-06-23 16:23:36 +0000

    Restyled by prettier

--- flaskapp/static/js/course/add_unit.js
+++ flaskapp/static/js/course/add_unit.js
@@ -1,67 +1,69 @@
-(function() {
-const chapterNo = document.getElementById("form__fields__chapter_no");
-const name = document.getElementById("form__fields__name");
-const formError = document.querySelectorAll(".form__error li");
-const clientError = document.getElementsByClassName("form__client_error");
-const form = document.getElementById("unit_form");
-const resetBtn = document.getElementById("reset_btn");
-chapterNo.addEventListener("input", () => { clientError[0].innerHTML = ""; });
-name.addEventListener("input", () => {
-  clientError[1].innerHTML = "";
-  for (let err of formError) {
-    err.innerHTML = "";
-  }
-});
-resetBtn.addEventListener("click", function() {
-  chapterNo.value = "";
-  name.value = "";
-  clientError[0].innerHTML = "";
-  clientError[1].innerHTML = "";
-});
-form.addEventListener("submit", function(e) {
-  let valid = {};
-  for (let err of clientError) {
-    err.innerHTML = "";
-  }
-  for (let err of formError) {
-    err.innerHTML = "";
-  }
-  valid = isValid(chapterNo.value, name.value);
-  if (!valid.isValid) {
-    e.preventDefault();
-    for (let i = 0; i < 2; i++) {
-      clientError[i].innerHTML = valid.error[i];
+(function () {
+  const chapterNo = document.getElementById("form__fields__chapter_no");
+  const name = document.getElementById("form__fields__name");
+  const formError = document.querySelectorAll(".form__error li");
+  const clientError = document.getElementsByClassName("form__client_error");
+  const form = document.getElementById("unit_form");
+  const resetBtn = document.getElementById("reset_btn");
+  chapterNo.addEventListener("input", () => {
+    clientError[0].innerHTML = "";
+  });
+  name.addEventListener("input", () => {
+    clientError[1].innerHTML = "";
+    for (let err of formError) {
+      err.innerHTML = "";
     }
-  }
-});
+  });
+  resetBtn.addEventListener("click", function () {
+    chapterNo.value = "";
+    name.value = "";
+    clientError[0].innerHTML = "";
+    clientError[1].innerHTML = "";
+  });
+  form.addEventListener("submit", function (e) {
+    let valid = {};
+    for (let err of clientError) {
+      err.innerHTML = "";
+    }
+    for (let err of formError) {
+      err.innerHTML = "";
+    }
+    valid = isValid(chapterNo.value, name.value);
+    if (!valid.isValid) {
+      e.preventDefault();
+      for (let i = 0; i < 2; i++) {
+        clientError[i].innerHTML = valid.error[i];
+      }
+    }
+  });
 
-function isValid(chapterNo, name) {
-  const valid = {
-    isValid : true,
-    error : [],
-  };
-  if (chapterNo === "") {
-    valid.isValid = false;
-    valid.error.push("chapter number is required!!");
-  } else if (isNaN(Number(chapterNo))) {
-    valid.isValid = false;
-    valid.error.push("chapter number must be integer");
-  } else if (Number(chapterNo) < 1 || Number(chapterNo) > 100) {
-    valid.isValid = false;
-    valid.error.push("chapter number must be between 1 and 100");
-  } else {
-    valid.error.push("");
-  }
+  function isValid(chapterNo, name) {
+    const valid = {
+      isValid: true,
+      error: [],
+    };
+    if (chapterNo === "") {
+      valid.isValid = false;
+      valid.error.push("chapter number is required!!");
+    } else if (isNaN(Number(chapterNo))) {
+      valid.isValid = false;
+      valid.error.push("chapter number must be integer");
+    } else if (Number(chapterNo) < 1 || Number(chapterNo) > 100) {
+      valid.isValid = false;
+      valid.error.push("chapter number must be between 1 and 100");
+    } else {
+      valid.error.push("");
+    }
 
-  if (name === "") {
-    valid.isValid = false;
-    valid.error.push("Chapter name is required!!");
-  } else if (name.length < 3) {
-    valid.isValid = false;
-    valid.error.push("name must have atleast three letters");
-  } else {
-    valid.error.push("");
+    if (name === "") {
+      valid.isValid = false;
+      valid.error.push("Chapter name is required!!");
+    } else if (name.length < 3) {
+      valid.isValid = false;
+      valid.error.push("name must have atleast three letters");
+    } else {
+      valid.error.push("");
+    }
+    return valid;
   }
-  return valid;
-}
 })();
--- flaskapp/static/js/papers/generate_paper.js
+++ flaskapp/static/js/papers/generate_paper.js
@@ -1,145 +1,166 @@
-(function() {
-const conflictsButton = document.getElementById("fix_conflicts");
-const logoContent = document.getElementById("paper_logo");
-const toggleContainer =
-    Array.from(document.getElementsByClassName("toggle_container"));
-const toggleButton = Array.from(document.getElementsByClassName("toggle_btn"));
-const addCheckbox = Array.from(document.getElementsByClassName("add_checkbox"));
-const qidContainer =
-    Array.from(document.getElementsByClassName("first_row_column_1"));
-const qids = qidContainer.map((node) => Number(node.getAttribute("data-id")));
-const qtypes = Array.from(document.getElementsByClassName("first_row_column_3"))
-                   .map((node) => node.innerHTML.trim());
-const logoForm = document.getElementById("logoform");
-const paperFields = Array.from(document.getElementsByClassName("paper_fields"));
-const formClientErrs =
-    Array.from(document.getElementsByClassName("form_client_error"));
-const cognitiveSkills =
-    Array.from(document.getElementsByClassName("cognitive"));
-const difficultyLevels =
-    Array.from(document.getElementsByClassName("difficulty"));
-const conflictsContainer = document.getElementById("conflicts_container");
+(function () {
+  const conflictsButton = document.getElementById("fix_conflicts");
+  const logoContent = document.getElementById("paper_logo");
+  const toggleContainer = Array.from(
+    document.getElementsByClassName("toggle_container")
+  );
+  const toggleButton = Array.from(
+    document.getElementsByClassName("toggle_btn")
+  );
+  const addCheckbox = Array.from(
+    document.getElementsByClassName("add_checkbox")
+  );
+  const qidContainer = Array.from(
+    document.getElementsByClassName("first_row_column_1")
+  );
+  const qids = qidContainer.map((node) => Number(node.getAttribute("data-id")));
+  const qtypes = Array.from(
+    document.getElementsByClassName("first_row_column_3")
+  ).map((node) => node.innerHTML.trim());
+  const logoForm = document.getElementById("logoform");
+  const paperFields = Array.from(
+    document.getElementsByClassName("paper_fields")
+  );
+  const formClientErrs = Array.from(
+    document.getElementsByClassName("form_client_error")
+  );
+  const cognitiveSkills = Array.from(
+    document.getElementsByClassName("cognitive")
+  );
+  const difficultyLevels = Array.from(
+    document.getElementsByClassName("difficulty")
+  );
+  const conflictsContainer = document.getElementById("conflicts_container");
 
-document.getElementById("page_display")
-    .firstElementChild.firstElementChild.innerHTML = "Conflicts";
-logoContent.style.display = "none";
+  document.getElementById(
+    "page_display"
+  ).firstElementChild.firstElementChild.innerHTML = "Conflicts";
+  logoContent.style.display = "none";
 
-qidContainer.forEach(function(node, index) { node.innerHTML = index + 1; });
-
-[cognitiveSkills, difficultyLevels].forEach((nodes) => {
-  nodes.forEach((node) => {
-    node.classList.add(node.innerHTML.toLowerCase().trim());
-    console.log(node);
+  qidContainer.forEach(function (node, index) {
+    node.innerHTML = index + 1;
   });
-});
 
-toggleContainer.forEach(function(node, index) {
-  node.addEventListener("click", function() {
-    if (addCheckbox[index].checked) {
-      toggleButton[index].style.left = "4px";
-      toggleContainer[index].style.backgroundColor = "#ccc";
-    } else {
-      toggleButton[index].style.left = "28px";
-      toggleContainer[index].style.backgroundColor = "rgba(144, 0, 240, 0.76)";
-    }
-    addCheckbox[index].checked = !addCheckbox[index].checked;
+  [cognitiveSkills, difficultyLevels].forEach((nodes) => {
+    nodes.forEach((node) => {
+      node.classList.add(node.innerHTML.toLowerCase().trim());
+      console.log(node);
+    });
   });
-});
 
-if (!addCheckbox.length) {
-  const data = {mcq : {ask : [], nask : []}, sub : {ask : [], nask : []}};
-  ajax(data);
-} else {
-
-  conflictsButton.addEventListener("click", function() {
-    const mcqAskId = qids.filter((node, index) => addCheckbox[index].checked &&
-                                                  qtypes[index] === "mcq");
-    const mcqNaskId =
-        qids.filter((node, index) =>
-                        !addCheckbox[index].checked && qtypes[index] === "mcq");
-    const subAskId = qids.filter((node, index) => addCheckbox[index].checked &&
-                                                  qtypes[index] === "sub");
-    const subNaskId =
-        qids.filter((node, index) =>
-                        !addCheckbox[index].checked && qtypes[index] === "sub");
-    const data = {
-      mcq : {
-        ask : mcqAskId,
-        nask : mcqNaskId,
-      },
-      sub : {
-        ask : subAskId,
-        nask : subNaskId,
-      },
-    };
-    ajax(data);
-  });
-}
-
-paperFields.forEach(function(node, index) {
-  node.addEventListener("input",
-                        function() { formClientErrs[index].innerHTML = ""; });
-});
-
-logoForm.addEventListener("submit", function(e) {
-  const values = paperFields.map((node) => node.value);
-  const status = isValid(values);
-  if (!status.isValid) {
-    formClientErrs.forEach(
-        (node, index) => { node.innerHTML = status.err[index]; });
-    e.preventDefault();
-  }
-});
-
-function isValid(values) {
-  let validation = {
-    isValid : true,
-    err : [],
-  };
-  values.forEach(function(value, index) {
-    if (index !== 4) {
-      if (!value) {
-        validation.isValid = false;
-        validation.err.push("required field!!");
+  toggleContainer.forEach(function (node, index) {
+    node.addEventListener("click", function () {
+      if (addCheckbox[index].checked) {
+        toggleButton[index].style.left = "4px";
+        toggleContainer[index].style.backgroundColor = "#ccc";
       } else {
-        validation.err.push("");
+        toggleButton[index].style.left = "28px";
+        toggleContainer[index].style.backgroundColor =
+          "rgba(144, 0, 240, 0.76)";
       }
-    } else {
-      if (!value) {
-        validation.isValid = false;
-        validation.err.push("required field!!");
-      } else {
-        const extension = value.substr(value.lastIndexOf(".") + 1);
-        console.log(extension);
-        const validExtensions = [ "jpg", "jpeg", "png", "tiff" ];
-        if (validExtensions.indexOf(extension) === -1) {
+      addCheckbox[index].checked = !addCheckbox[index].checked;
+    });
+  });
+
+  if (!addCheckbox.length) {
+    const data = { mcq: { ask: [], nask: [] }, sub: { ask: [], nask: [] } };
+    ajax(data);
+  } else {
+    conflictsButton.addEventListener("click", function () {
+      const mcqAskId = qids.filter(
+        (node, index) => addCheckbox[index].checked && qtypes[index] === "mcq"
+      );
+      const mcqNaskId = qids.filter(
+        (node, index) => !addCheckbox[index].checked && qtypes[index] === "mcq"
+      );
+      const subAskId = qids.filter(
+        (node, index) => addCheckbox[index].checked && qtypes[index] === "sub"
+      );
+      const subNaskId = qids.filter(
+        (node, index) => !addCheckbox[index].checked && qtypes[index] === "sub"
+      );
+      const data = {
+        mcq: {
+          ask: mcqAskId,
+          nask: mcqNaskId,
+        },
+        sub: {
+          ask: subAskId,
+          nask: subNaskId,
+        },
+      };
+      ajax(data);
+    });
+  }
+
+  paperFields.forEach(function (node, index) {
+    node.addEventListener("input", function () {
+      formClientErrs[index].innerHTML = "";
+    });
+  });
+
+  logoForm.addEventListener("submit", function (e) {
+    const values = paperFields.map((node) => node.value);
+    const status = isValid(values);
+    if (!status.isValid) {
+      formClientErrs.forEach((node, index) => {
+        node.innerHTML = status.err[index];
+      });
+      e.preventDefault();
+    }
+  });
+
+  function isValid(values) {
+    let validation = {
+      isValid: true,
+      err: [],
+    };
+    values.forEach(function (value, index) {
+      if (index !== 4) {
+        if (!value) {
           validation.isValid = false;
-          validation.err.push("enter valid image file");
+          validation.err.push("required field!!");
         } else {
           validation.err.push("");
         }
+      } else {
+        if (!value) {
+          validation.isValid = false;
+          validation.err.push("required field!!");
+        } else {
+          const extension = value.substr(value.lastIndexOf(".") + 1);
+          console.log(extension);
+          const validExtensions = ["jpg", "jpeg", "png", "tiff"];
+          if (validExtensions.indexOf(extension) === -1) {
+            validation.isValid = false;
+            validation.err.push("enter valid image file");
+          } else {
+            validation.err.push("");
+          }
+        }
       }
-    }
-  });
-  return validation;
-}
+    });
+    return validation;
+  }
 
-function ajax(data) {
-  fetch("/papers/handle/conflicts", {
-    method : "post",
-    headers : {
-      "Content-Type" : "application/json",
-    },
-    body : JSON.stringify(data),
-  })
+  function ajax(data) {
+    fetch("/papers/handle/conflicts", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
       .then((res) => res.json())
       .then((res) => {
-        const {status} = res;
+        const { status } = res;
         if (status === "OK") {
           logoContent.style.display = "";
           conflictsContainer.style.display = "none";
         }
       })
-      .catch((err) => { console.log(err); });
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 })();
