1265abf190f979a1235b5d3350722edad81b5380
Restyled.io <commits@restyled.io>
2020-06-23 16:23:03 +0000

    Restyled by isort

--- migrations/versions/9ea370b41a33_migration_message.py
+++ migrations/versions/9ea370b41a33_migration_message.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2020-06-23 21:47:29.299747
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "9ea370b41a33"
