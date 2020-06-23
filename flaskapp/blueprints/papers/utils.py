d08d897798508a9d41bf4173b4b93a7bef83a238
Restyled.io <commits@restyled.io>
2020-06-23 14:44:07 +0000

    Restyled by black

--- flaskapp/blueprints/papers/utils.py
+++ flaskapp/blueprints/papers/utils.py
@@ -14,10 +14,16 @@ class QuestionNotFoundError(Exception):
 
 
 def find_conflicting_questions(course_id, constraints):
-    unit = (db.session.query(Unit).filter(
-            and_(Unit.chapter_no == constraints["unit"],
-                 Unit.course_id == course_id)).first())
-    return (db.session.query(Question).filter(
+    unit = (
+        db.session.query(Unit)
+        .filter(
+            and_(Unit.chapter_no == constraints["unit"], Unit.course_id == course_id)
+        )
+        .first()
+    )
+    return (
+        db.session.query(Question)
+        .filter(
             and_(
                 Question.cognitive_level == constraints["cognitive"],
                 Question.difficulty == constraints["difficulty"],
@@ -26,33 +32,54 @@ def find_conflicting_questions(course_id, constraints):
                 Question.question_type == constraints["question_type"],
                 Question.imp is True,
                 Question.is_asked is True,
-            )).all())
+            )
+        )
+        .all()
+    )
 
 
 def find_random_question(course_id, constraints):
-    unit = (db.session.query(Unit).filter(
-            and_(Unit.chapter_no == constraints["unit"],
-                 Unit.course_id == course_id)).first())
-    imp_question = (db.session.query(Question).filter(
-        and_(
-                    Question.cognitive_level == constraints["cognitive"],
-                    Question.difficulty == constraints["difficulty"],
-                    Question.mark == constraints["mark"],
-                    Question.unit_id == unit.id,
-                    Question.imp is True,
-                    Question.question_type == constraints["question_type"]
-                    )).order_by(func.random()).first().id)
+    unit = (
+        db.session.query(Unit)
+        .filter(
+            and_(Unit.chapter_no == constraints["unit"], Unit.course_id == course_id)
+        )
+        .first()
+    )
+    imp_question = (
+        db.session.query(Question)
+        .filter(
+            and_(
+                Question.cognitive_level == constraints["cognitive"],
+                Question.difficulty == constraints["difficulty"],
+                Question.mark == constraints["mark"],
+                Question.unit_id == unit.id,
+                Question.imp is True,
+                Question.question_type == constraints["question_type"],
+            )
+        )
+        .order_by(func.random())
+        .first()
+        .id
+    )
     if imp_question:
         return imp_question.to_dict()
-    question = (db.session.query(Question).filter(
-        and_(
+    question = (
+        db.session.query(Question)
+        .filter(
+            and_(
                 Question.cognitive_level == constraints["cognitive"],
                 Question.difficulty == constraints["difficulty"],
                 Question.mark == constraints["mark"],
                 Question.unit_id == unit.id,
                 Question.is_asked is False,
-                Question.question_type == constraints["question_type"]
-                )).order_by(func.random()).first().id)
+                Question.question_type == constraints["question_type"],
+            )
+        )
+        .order_by(func.random())
+        .first()
+        .id
+    )
     if question:
         return question.to_dict()
     raise QuestionNotFoundError()
@@ -69,8 +96,7 @@ def save_logo(form_picture):
     """
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = secrets.token_urlsafe(10) + f_ext
-    picture_path = os.path.join(current_app.root_path, "static/logos",
-                                picture_fn)
+    picture_path = os.path.join(current_app.root_path, "static/logos", picture_fn)
 
     output_size = (400, 400)
     i = Image.open(form_picture)
