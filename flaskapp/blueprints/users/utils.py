cc4fe4c009658ec71bda8b79a86865276fa85330
Restyled.io <commits@restyled.io>
2020-06-26 18:36:59 +0000

    Restyled by isort

--- flaskapp/blueprints/users/utils.py
+++ flaskapp/blueprints/users/utils.py
@@ -1,14 +1,10 @@
 import os
 
-from flask import current_app
-from flask import render_template
-from flask import url_for
+from flask import current_app, render_template, url_for
 from flask_login import current_user
 from flask_mail import Message
+from flaskapp import APP_PATH, mail
 from PIL import Image
-
-from flaskapp import APP_PATH
-from flaskapp import mail
 
 
 def save_picture(form_picture):
