9523d0ae440c7b8f344a23d75816e9031e51886c
patel jeel <42006763+jeel2308@users.noreply.github.com>
2020-06-27 19:57:57 +0530

    change confirm message

--- flaskapp/static/js/papers/confirm_paper_template.js
+++ flaskapp/static/js/papers/confirm_paper_template.js
@@ -89,7 +89,7 @@
     fetch(`/course/${courseId}/papers/confirm/template/`, {
       method: "post",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(markDistribution),
+      body: JSON.stringify({ status:"OK" }),
     })
       .then((val) => val.text())
       .then((paper) => {
