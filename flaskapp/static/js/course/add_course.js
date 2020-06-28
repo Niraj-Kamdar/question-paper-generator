8fb3946744680433aff6ef8dfb31c93043b93dea
Restyled.io <commits@restyled.io>
2020-06-28 20:24:59 +0000

    Restyled by prettier

--- flaskapp/static/js/course/add_course.js
+++ flaskapp/static/js/course/add_course.js
@@ -1,59 +1,58 @@
-(function() {
-const courseField = document.getElementById("form__fields__course");
-const clientError = document.getElementsByClassName("form__client_error");
-const formError = document.querySelectorAll(".form__error li");
-const courseForm = document.getElementById("course_form");
-const toggleContainer = document.getElementById("toggle_container");
-const toggleBtn = document.getElementById("toggle_btn");
-const impCheckbox = document.getElementById("checkbox");
-const submitBtn = document.getElementById("submit_btn");
-const resetBtn = document.getElementById("reset_btn");
+(function () {
+  const courseField = document.getElementById("form__fields__course");
+  const clientError = document.getElementsByClassName("form__client_error");
+  const formError = document.querySelectorAll(".form__error li");
+  const courseForm = document.getElementById("course_form");
+  const toggleContainer = document.getElementById("toggle_container");
+  const toggleBtn = document.getElementById("toggle_btn");
+  const impCheckbox = document.getElementById("checkbox");
+  const submitBtn = document.getElementById("submit_btn");
+  const resetBtn = document.getElementById("reset_btn");
 
-let tabindex = 1;
-courseField.setAttribute("tabindex", tabindex++);
-toggleContainer.setAttribute("tabindex", tabindex++);
-submitBtn.setAttribute("tabindex", tabindex++);
-resetBtn.setAttribute("tabindex", tabindex++);
+  let tabindex = 1;
+  courseField.setAttribute("tabindex", tabindex++);
+  toggleContainer.setAttribute("tabindex", tabindex++);
+  submitBtn.setAttribute("tabindex", tabindex++);
+  resetBtn.setAttribute("tabindex", tabindex++);
 
-function isValid(course) {
-  let validation = {
-    isValid : true,
-    errors : [],
-  };
-  if (course.trim() === "") {
-    validation.errors.push("required field");
-    validation.isValid = false;
-  } else
-    validation.errors.push("");
-  return validation;
-}
-
-courseField.setAttribute("placeholder", "Add course");
-courseField.addEventListener("input", () => {
-  clientError[0].innerHTML = "";
-  if (formError.length) {
-    formError[0].innerHTML = "";
+  function isValid(course) {
+    let validation = {
+      isValid: true,
+      errors: [],
+    };
+    if (course.trim() === "") {
+      validation.errors.push("required field");
+      validation.isValid = false;
+    } else validation.errors.push("");
+    return validation;
   }
-});
 
-toggleContainer.addEventListener("click", () => {
-  if (impCheckbox.checked) {
-    toggleBtn.style.left = "4px";
-    toggleContainer.style.backgroundColor = "#ccc";
-  } else {
-    toggleBtn.style.left = "28px";
-    toggleContainer.style.backgroundColor = "rgba(144, 0, 240, 0.76)";
-  }
-  impCheckbox.checked = !impCheckbox.checked;
-});
+  courseField.setAttribute("placeholder", "Add course");
+  courseField.addEventListener("input", () => {
+    clientError[0].innerHTML = "";
+    if (formError.length) {
+      formError[0].innerHTML = "";
+    }
+  });
 
-courseForm.addEventListener("submit", (e) => {
-  const validation = isValid(courseField.value);
-  clientError[0].innerHTML = "";
+  toggleContainer.addEventListener("click", () => {
+    if (impCheckbox.checked) {
+      toggleBtn.style.left = "4px";
+      toggleContainer.style.backgroundColor = "#ccc";
+    } else {
+      toggleBtn.style.left = "28px";
+      toggleContainer.style.backgroundColor = "rgba(144, 0, 240, 0.76)";
+    }
+    impCheckbox.checked = !impCheckbox.checked;
+  });
 
-  if (!validation.isValid) {
-    e.preventDefault();
-    clientError[0].innerText = validation.errors[0];
-  }
-});
+  courseForm.addEventListener("submit", (e) => {
+    const validation = isValid(courseField.value);
+    clientError[0].innerHTML = "";
+
+    if (!validation.isValid) {
+      e.preventDefault();
+      clientError[0].innerText = validation.errors[0];
+    }
+  });
 })();
--- flaskapp/static/js/papers/mark_distribution_form.js
+++ flaskapp/static/js/papers/mark_distribution_form.js
@@ -1,158 +1,180 @@
-(function() {
-const markForm = document.getElementById("mark_form");
-const hiddenLabel = Array.from(document.getElementsByClassName("hidden_label"));
-const labels = Array.from(document.getElementsByClassName("label"));
-const inputField = Array.from(document.getElementsByClassName("input_field"));
-const backBtn = Array.from(document.getElementsByClassName("back_btn"));
-const nextBtn = Array.from(document.getElementsByClassName("next_btn"));
-const pages = document.getElementsByClassName("pages");
-const marks = Number(document.getElementById("total_marks").innerHTML);
-const unitFields = Array.from(document.getElementsByClassName("unit"));
-const cognitiveFields =
-    Array.from(document.getElementsByClassName("cognitive"));
-const difficultyFields =
-    Array.from(document.getElementsByClassName("difficulty"));
-const questionsFields = Array.from(document.getElementsByClassName("question"));
-const questionTypeFields =
-    Array.from(document.getElementsByClassName("question_type"));
-const unitFieldsErr = Array.from(document.getElementsByClassName("unit_err"));
-const cognitiveFieldsErr =
-    Array.from(document.getElementsByClassName("cognitive_err"));
-const difficultyFieldsErr =
-    Array.from(document.getElementsByClassName("difficulty_err"));
-const questionTypeFieldsErr =
-    Array.from(document.getElementsByClassName("question_type_err"));
-const questionsFieldsErr =
-    Array.from(document.getElementsByClassName("question_err"));
-const marksErr = document.getElementsByClassName("marks_err");
-const header = document.getElementsByClassName("header")[0];
-const fieldHeader = Array.from(document.getElementsByClassName("field_header"));
-const formParts = [
-  unitFields,
-  cognitiveFields,
-  difficultyFields,
-  questionTypeFields,
-  questionsFields,
-];
-const formPartsErr = [
-  unitFieldsErr,
-  cognitiveFieldsErr,
-  difficultyFieldsErr,
-  questionTypeFieldsErr,
-  questionsFieldsErr,
-];
+(function () {
+  const markForm = document.getElementById("mark_form");
+  const hiddenLabel = Array.from(
+    document.getElementsByClassName("hidden_label")
+  );
+  const labels = Array.from(document.getElementsByClassName("label"));
+  const inputField = Array.from(document.getElementsByClassName("input_field"));
+  const backBtn = Array.from(document.getElementsByClassName("back_btn"));
+  const nextBtn = Array.from(document.getElementsByClassName("next_btn"));
+  const pages = document.getElementsByClassName("pages");
+  const marks = Number(document.getElementById("total_marks").innerHTML);
+  const unitFields = Array.from(document.getElementsByClassName("unit"));
+  const cognitiveFields = Array.from(
+    document.getElementsByClassName("cognitive")
+  );
+  const difficultyFields = Array.from(
+    document.getElementsByClassName("difficulty")
+  );
+  const questionsFields = Array.from(
+    document.getElementsByClassName("question")
+  );
+  const questionTypeFields = Array.from(
+    document.getElementsByClassName("question_type")
+  );
+  const unitFieldsErr = Array.from(document.getElementsByClassName("unit_err"));
+  const cognitiveFieldsErr = Array.from(
+    document.getElementsByClassName("cognitive_err")
+  );
+  const difficultyFieldsErr = Array.from(
+    document.getElementsByClassName("difficulty_err")
+  );
+  const questionTypeFieldsErr = Array.from(
+    document.getElementsByClassName("question_type_err")
+  );
+  const questionsFieldsErr = Array.from(
+    document.getElementsByClassName("question_err")
+  );
+  const marksErr = document.getElementsByClassName("marks_err");
+  const header = document.getElementsByClassName("header")[0];
+  const fieldHeader = Array.from(
+    document.getElementsByClassName("field_header")
+  );
+  const formParts = [
+    unitFields,
+    cognitiveFields,
+    difficultyFields,
+    questionTypeFields,
+    questionsFields,
+  ];
+  const formPartsErr = [
+    unitFieldsErr,
+    cognitiveFieldsErr,
+    difficultyFieldsErr,
+    questionTypeFieldsErr,
+    questionsFieldsErr,
+  ];
 
-hiddenLabel.forEach(function(node) { node.style.display = "none"; });
-
-labels.forEach(function(node, index) {
-  node.setAttribute("for", hiddenLabel[index].getAttribute("for"));
-  node.innerHTML = hiddenLabel[index].innerHTML;
-});
-
-inputField.forEach(function(node, index) {
-  const forAttribute = hiddenLabel[index].getAttribute("for");
-  node.setAttribute("id", forAttribute);
-  node.setAttribute("name", forAttribute);
-});
-
-fieldHeader.forEach(function(node) { node.style.display = "none"; });
-
-Array.from(pages).forEach(function(node, index) {
-  if (index) {
+  hiddenLabel.forEach(function (node) {
     node.style.display = "none";
-  }
-});
-header.innerHTML = fieldHeader[0].innerHTML;
+  });
 
-markForm.addEventListener("submit",
-                          function(e) { navigationHandler(4, false, e); });
+  labels.forEach(function (node, index) {
+    node.setAttribute("for", hiddenLabel[index].getAttribute("for"));
+    node.innerHTML = hiddenLabel[index].innerHTML;
+  });
 
-formParts.forEach(function(element, index) {
-  element.forEach(function(node, index2) {
-    node.addEventListener("input", function() {
-      formPartsErr[index][index2].innerHTML = "";
-      marksErr[index].innerHTML = "";
+  inputField.forEach(function (node, index) {
+    const forAttribute = hiddenLabel[index].getAttribute("for");
+    node.setAttribute("id", forAttribute);
+    node.setAttribute("name", forAttribute);
+  });
+
+  fieldHeader.forEach(function (node) {
+    node.style.display = "none";
+  });
+
+  Array.from(pages).forEach(function (node, index) {
+    if (index) {
+      node.style.display = "none";
+    }
+  });
+  header.innerHTML = fieldHeader[0].innerHTML;
+
+  markForm.addEventListener("submit", function (e) {
+    navigationHandler(4, false, e);
+  });
+
+  formParts.forEach(function (element, index) {
+    element.forEach(function (node, index2) {
+      node.addEventListener("input", function () {
+        formPartsErr[index][index2].innerHTML = "";
+        marksErr[index].innerHTML = "";
+      });
     });
   });
-});
 
-nextBtn.forEach(function(node, index) {
-  if (index !== 4) {
-    node.addEventListener("click",
-                          function() { navigationHandler(index, false); });
-  }
-});
-
-backBtn.forEach(function(node, index) {
-  node.addEventListener("click",
-                        function() { navigationHandler(index, true); });
-});
-
-function isValidNumber(number) {
-  let isValid = false;
-  let err = "";
-  if (number === "") {
-    err = "Enter Marks in Integers";
-  } else if (Number(number) < 0) {
-    err = "Marks should be non-negative";
-  } else {
-    const indexOfDecimal = number.indexOf(".");
-    const indexOfExp = number.indexOf("e");
-    if (indexOfDecimal !== -1 || indexOfExp !== -1) {
-      err = "Enter Marks in Integers";
+  nextBtn.forEach(function (node, index) {
+    if (index !== 4) {
+      node.addEventListener("click", function () {
+        navigationHandler(index, false);
+      });
     }
-  }
-  if (!err) {
-    isValid = true;
-  }
-  return [ isValid, err ];
-}
+  });
 
-function isValidTotalMarks(element) {
-  const total_marks =
-      element.map((node) => Number(node.value)).reduce((acc, cur) => acc + cur);
-  return total_marks === marks;
-}
-
-function isErrExists(...element) {
-  return element.map((node) => node.innerHTML).reduce((acc, cur) => acc + cur);
-}
-
-function navigationHandler(index, back, event) {
-  if (back)
-    index++;
-  const s = isErrExists(...formPartsErr[index], marksErr[index]);
-  if (!s) {
-    let validFields = 0;
-    formParts[index].forEach(function(node, index2) {
-      const [isValid, err] = isValidNumber(node.value);
-      if (!isValid) {
-        formPartsErr[index][index2].innerHTML = err;
-      } else {
-        validFields++;
-      }
+  backBtn.forEach(function (node, index) {
+    node.addEventListener("click", function () {
+      navigationHandler(index, true);
     });
-    const status = isValidTotalMarks(formParts[index]);
-    if (status) {
-      validFields++;
+  });
+
+  function isValidNumber(number) {
+    let isValid = false;
+    let err = "";
+    if (number === "") {
+      err = "Enter Marks in Integers";
+    } else if (Number(number) < 0) {
+      err = "Marks should be non-negative";
     } else {
-      marksErr[index].innerHTML = "Total marks are not equal to paper marks!!";
-    }
-    if (validFields === formParts[index].length + 1) {
-      pages[index].style.display = "none";
-      if (back) {
-        pages[index - 1].style.display = "";
-        header.innerHTML = fieldHeader[index - 1].innerHTML;
-      } else {
-        if (index + 1 < 5) {
-          pages[index + 1].style.display = "";
-          header.innerHTML = fieldHeader[index + 1].innerHTML;
-        }
+      const indexOfDecimal = number.indexOf(".");
+      const indexOfExp = number.indexOf("e");
+      if (indexOfDecimal !== -1 || indexOfExp !== -1) {
+        err = "Enter Marks in Integers";
       }
-    } else {
-      if (event)
-        event.preventDefault();
+    }
+    if (!err) {
+      isValid = true;
+    }
+    return [isValid, err];
+  }
+
+  function isValidTotalMarks(element) {
+    const total_marks = element
+      .map((node) => Number(node.value))
+      .reduce((acc, cur) => acc + cur);
+    return total_marks === marks;
+  }
+
+  function isErrExists(...element) {
+    return element
+      .map((node) => node.innerHTML)
+      .reduce((acc, cur) => acc + cur);
+  }
+
+  function navigationHandler(index, back, event) {
+    if (back) index++;
+    const s = isErrExists(...formPartsErr[index], marksErr[index]);
+    if (!s) {
+      let validFields = 0;
+      formParts[index].forEach(function (node, index2) {
+        const [isValid, err] = isValidNumber(node.value);
+        if (!isValid) {
+          formPartsErr[index][index2].innerHTML = err;
+        } else {
+          validFields++;
+        }
+      });
+      const status = isValidTotalMarks(formParts[index]);
+      if (status) {
+        validFields++;
+      } else {
+        marksErr[index].innerHTML =
+          "Total marks are not equal to paper marks!!";
+      }
+      if (validFields === formParts[index].length + 1) {
+        pages[index].style.display = "none";
+        if (back) {
+          pages[index - 1].style.display = "";
+          header.innerHTML = fieldHeader[index - 1].innerHTML;
+        } else {
+          if (index + 1 < 5) {
+            pages[index + 1].style.display = "";
+            header.innerHTML = fieldHeader[index + 1].innerHTML;
+          }
+        }
+      } else {
+        if (event) event.preventDefault();
+      }
     }
   }
-}
 })();
