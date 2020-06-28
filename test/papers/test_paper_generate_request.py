bf407125e485d36cbed1d5430bb5fd3bc5907741
Jaymin <60779004+201701203@users.noreply.github.com>
2020-06-28 14:50:19 +0530

    Update test_paper_generate_request.py

--- test/papers/test_paper_generate_request.py
+++ test/papers/test_paper_generate_request.py
@@ -4,6 +4,7 @@ from flaskapp.models import Paper
 from test.main.base_classes import BaseMCQQuestion
 from test.main.base_classes import BaseSubQuestion
 from test.main.utils import test_post_request
+import pyPdf
 
 class PaperGenerateRequest(BaseSubQuestion, BaseMCQQuestion):
     def test_paper_generate_request(self):
@@ -93,4 +94,4 @@ class PaperGenerateRequest(BaseSubQuestion, BaseMCQQuestion):
         self.test_mark_distribution_form()
         self.test_generate_and_confirm_paper()
         response = self.client.post('/papers/1')
-        self.assertIn(b"<title>Paper-to-PDF</title>", response.data)
+        pdf = pyPdf.PdfFileReader(open(response, "rb"))
